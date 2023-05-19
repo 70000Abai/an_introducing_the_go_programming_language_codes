68bc0c518e15748d499df76ec1ea29cc87ef2a43
70000Abai <132120480+70000Abai@users.noreply.github.com>
2023-05-19 13:05:34 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import "fmt"
+import ("fmt")
 
-func main (){
-	fmt.Println(true && false)
-	fmt.Println(false && true)
-	fmt.Println (!false && true)
+
+
+func main () {
+	fmt.Println("32132 + 42452=" , 32132 * 42452)
 }
